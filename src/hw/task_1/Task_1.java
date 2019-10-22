2db3875aba04fabfcf4f3fe5fa680b5845e60658
MrJoinsSS <polhyta@mail.ru>
2019-10-22 22:04:50 +0300

    init commit

---
+++ src/hw/task_1/Task_1.java
@@ -0,0 +1,24 @@
+package hw.task_1;
+
+public class Task_1 {
+    public static void main(String[] args) {
+
+
+        int number = 324;
+
+        if (number >= 0){
+            System.out.println("Number is positive");
+        } else {
+            System.out.println("Number is negative");
+            number = number*(-1);
+        }
+
+        if (number >= 0 && number<10){
+            System.out.println("Single digit");
+        } else if(number > 10 && number<100){
+            System.out.println("Double digit");
+        } else if(number > 100 && number<1000){
+            System.out.println("Three-digit number");
+        }
+    }
+}
