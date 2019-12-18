b859df4b56b2c5fac19c5c13a4c271ade6abf307
MrJoinsSS <polhyta@mail.ru>
2019-12-18 23:42:35 +0300

    task_4 - last task

--- src/hw_6/task_2/Auto.java
+++ src/hw_6/task_2/Auto.java
@@ -1,4 +1,30 @@
 package hw_6.task_2;
 
-public class Auto {
+
+public class Auto implements Car{
+    private boolean engineRunning = false;
+    private int gear = 0;
+
+
+
+    @Override
+    public boolean startEngine() {
+        return false;
+    }
+
+    @Override
+    public boolean stopEngine() {
+        return false;
+    }
+
+    @Override
+    public int switchTransmissionUp() {
+
+        return 0;
+    }
+
+    @Override
+    public int switchTransmissionDown() {
+        return 0;
+    }
 }
---
+++ src/hw_6/task_2/Car.java
@@ -0,0 +1,10 @@
+package hw_6.task_2;
+
+public interface Car {
+    boolean startEngine();
+    boolean stopEngine();
+    int switchTransmissionUp();
+    int switchTransmissionDown();
+
+
+}
